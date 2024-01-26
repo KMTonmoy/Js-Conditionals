0a98d80b5cd62c83e9ebeafdd2664d1b21852927
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-01-26 20:40:55 +0600

    Comparision File Compleate

---
+++ comparision.js
@@ -0,0 +1,36 @@
+// console.log(5 > 10)
+// console.log(5 < 10)
+// console.log(5 == 10)
+// console.log(10 == 10)
+// console.log(5 <= 10);
+// console.log(5 >= 10);
+// console.log(10 <= 10);
+// console.log(100 <= 10);
+
+// console.log(10 == 10);
+// console.log(10 == 10.1);
+
+// * ----------------------------------------------
+var PeyraPrice = 40;
+var applePrice = 250;
+
+// console.log(PeyraPrice > applePrice)
+
+console.log(applePrice < PeyraPrice);
+
+/**
+ * ==
+ * ===
+ * */
+
+console.log(10 == "10");
+console.log(10 === "10");
+
+console.log(120 === "tomato");
+
+//===========================================
+
+console.log(10 != "10");
+
+console.log(10 !== "10");
+
