7d7047c223c7861d0f4eecf29564f94612d41bda
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-01-26 20:59:14 +0600

    if.js File compleate

---
+++ if.js
@@ -0,0 +1,61 @@
+/**
+ * Tui Jodi amake Khawas
+ * Thalole ami asbo
+ * =======================
+ * if you buy food for me
+ *          I will come
+ *
+ * ===========================
+ * if biriyany price less then 300 taka
+ *              I will Buy
+ *
+ *
+ * ================Structure==========================
+ *
+ * if(condition){
+ *
+ * }
+ *
+ * ==================================================
+ *
+ * if (condition){
+ *
+ * }
+ *
+ *
+ *
+ *
+ * */
+
+if (13 < 10) {
+  console.log("I have a Smaller Vlue");
+}
+
+var biriyaniPrice = 500;
+
+biriyaniPrice = 250;
+
+if (biriyaniPrice < 300) {
+  console.log("mama Give me some Biriyany");
+}
+
+
+
+/***
+ * if(condition){
+ *  Exicute code if condition above is true
+ * }else{
+ *          Exicute code if condition is false
+ * }
+ *
+ * **/ 
+
+
+var weight = 20;
+
+if(weight < 20){
+    console.log('I will carry it by Myselsf')
+
+}else{
+    console.log("I will rent a rickshaw")
+}
