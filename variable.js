56ced65a94637c2c0d90d3987290c352e93f01b2
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-01-26 20:24:55 +0600

    aunty File Compleate

---
+++ variable.js
@@ -0,0 +1,35 @@
+/**
+ * VARIABLE ==> Container
+ *
+ * */
+// Number
+var sunglassPrice = 80;
+
+// String
+var subject = "EEE";
+
+// Boolean
+var passed = true;
+
+
+/**
+ * Naming Converention
+ * 1. single word 
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. Prefer camelCase
+ *  */ 
+
+var lastExamResult = 89
+
+/**
+ * OPERATION
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN 
+ * 4. null
+ * 5. Number
+ * 
+ * */ 
